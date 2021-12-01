389baf3572d68982b9cda4e2b7ea512302fb475e
Sara Hackle <Shackle@ivytech.edu>
2021-12-01 09:36:08 -0600

    Initial Commit, four ways to write functions

---
+++ favegame.js
@@ -0,0 +1,20 @@
+let makeFaveGameString = (faveGame) => {
+  return `My favorite game is ${faveGame}`;
+};
+
+console.log(makeFaveGameString("borderlands"));
+
+makeFaveGameString = (faveGame) => `My favorite game is ${faveGame}`;
+
+console.log(makeFaveGameString("monopoly"));
+
+makeFaveGameString = (faveGame) => {
+  faveGameString = `My favorite game is ${faveGame}`;
+  return faveGameString;
+};
+
+console.log(makeFaveGameString("Dark Souls"));
+
+makeFaveGameString = (faveGame) => `My favorite game is ${faveGame}`;
+
+console.log(makeFaveGameString("CandyLand"));
